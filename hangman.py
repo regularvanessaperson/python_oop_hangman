3f565fefbf25c23c210a089d437f75a201feecc3
regularvanessaperson <vamavila4@gmail.com>
2021-01-17 14:54:25 -0600

    guessed character shows up if correct

--- hangman.py
+++ hangman.py
@@ -7,10 +7,14 @@ class Word():
         iterator = iter(self.characters)
         self.guess_dictionary = dict(zip(iterator, initalState))
     def guess_attempt (self, characters):
+        self.turns = 10
+        self.failed = 0
         if characters in self.guess_dictionary and self.guess_dictionary[characters]== False:
             self.guess_dictionary[characters] = True
             return True
         else:
+            turns -= 1
+            failed += 1
             print("This letter is not in the word or you have already guessed it.")
             return False
     
@@ -21,8 +25,22 @@ class Word():
             else: 
                 print(characters, end = ' ')
         print('')
-
+    # def turn_counter(self, characters):
+    #     self.turns = 10
+    #     while turns > 0:
+    #         self.failed = 0
+    #         for characters in self.chosen_word:
+    #             if self.guess_dictionary[characters]== False:
+    #                 turns -= 1
+    #                 failed += 1
+    #                print(print_word()) 
+    #             else:
+    #                print(print_word()) 
 
 new_word = Word("TREE")
+
+guess = input("Guess a character:")
+ 
+new_word.guess_attempt(guess)
 
 new_word.print_word()
