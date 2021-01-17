223220e48264cf09744bb1939843aff4ae7892b4
regularvanessaperson <vamavila4@gmail.com>
2021-01-17 13:34:19 -0600

    printing word as dashes

--- hangman.py
+++ hangman.py
@@ -3,12 +3,26 @@ class Word():
         self.chosen_word = chosen_word
         self.characters = list(chosen_word)
         self.spaces = len(self.characters)
-        
-    # def guess(self, characters):
-    #     if characters in 
-        
+        initalState = [False for i in range(self.spaces)]
+        iterator = iter(self.characters)
+        self.guess_dictionary = dict(zip(iterator, initalState))
+    def guess_attempt (self, characters):
+        if characters in self.guess_dictionary and self.guess_dictionary[characters]== False:
+            self.guess_dictionary[characters] = True
+            return True
+        else:
+            print("This letter is not in the word or you have already guessed it.")
+            return False
+    
+    def print_word(self):
+        for characters in self.chosen_word:
+            if self.guess_dictionary[characters] == False:
+                print("_", end = ' ')
+            else: 
+                print(characters, end = ' ')
+        print('')
 
 
-new_word = Word("CHILI")
+new_word = Word("TREE")
 
-print(new_word.characters)
+new_word.print_word()
