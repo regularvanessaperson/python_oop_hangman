eea036a24fbcc517fe09e7a9309fe5e769c009df
regularvanessaperson <vamavila4@gmail.com>
2021-01-17 15:53:59 -0600

    able to type letters and display turns need to add conditions to win and lose the game next

--- hangman.py
+++ hangman.py
@@ -3,19 +3,19 @@ class Word():
         self.chosen_word = chosen_word
         self.characters = list(chosen_word)
         self.spaces = len(self.characters)
+        self.turns = 10
+        self.failed = 0
         initalState = [False for i in range(self.spaces)]
         iterator = iter(self.characters)
         self.guess_dictionary = dict(zip(iterator, initalState))
     def guess_attempt (self, characters):
-        self.turns = 10
-        self.failed = 0
         if characters in self.guess_dictionary and self.guess_dictionary[characters]== False:
             self.guess_dictionary[characters] = True
             return True
         else:
-            turns -= 1
-            failed += 1
-            print("This letter is not in the word or you have already guessed it.")
+            self.turns -= 1
+            self.failed += 1
+            print(f"This letter is not in the word or you have already guessed it, you have {self.turns} turns left")
             return False
     
     def print_word(self):
@@ -25,22 +25,23 @@ class Word():
             else: 
                 print(characters, end = ' ')
         print('')
-    # def turn_counter(self, characters):
-    #     self.turns = 10
-    #     while turns > 0:
-    #         self.failed = 0
-    #         for characters in self.chosen_word:
-    #             if self.guess_dictionary[characters]== False:
-    #                 turns -= 1
-    #                 failed += 1
-    #                print(print_word()) 
-    #             else:
-    #                print(print_word()) 
+ 
 
 new_word = Word("TREE")
 
 guess = input("Guess a character:")
- 
-new_word.guess_attempt(guess)
 
-new_word.print_word()
+
+
+
+
+while new_word.turns > 0: 
+    new_word.guess_attempt(guess)
+    if new_word.guess_attempt(guess) == True:
+        new_word.print_word()
+        guess = input("Guess a character:")
+    else:
+        new_word.print_word()
+        guess = input("Guess a character:")
+
+
